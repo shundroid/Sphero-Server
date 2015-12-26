eeee43c91405d246bc86060cd6777891fab21277
shundroid <ra24483@qb3.so-net.ne.jp>
2015-12-26 10:28:40 +0900

    update main.js

--- main.js
+++ main.js
@@ -1,16 +1,26 @@
 "use strict";
 
 var sphero = require("sphero");
+var http = require("http");
+var websocket = require("websocket");
 
 var isRed = false;
 var setting;
 var fs = require("fs");
 var orb;
-function readSetting() {
+var server;
+
+function main() {
+  readSetting(connectSphero);
+  createServer();
+  initWSServer();
+}
+
+function readSetting(connectCallback) {
   fs.readFile(__dirname + '/public_html/settings.json', 'utf-8', function(err, data) {
     setting = JSON.parse(data);
     orb = sphero(setting.sphero_serialport);
-    orb.connect(connectSphero);
+    orb.connect(connectCallback);
   });
 }
 var mimes = {
@@ -20,9 +30,7 @@ var mimes = {
   "json": "application/json"
 }
 function createServer() {
-  var http = require("http");
-  var ws = require("websocket").server;
-  var server = http.createServer(function(req, res) {
+  server = http.createServer(function(req, res) {
     var access_to = req.url;
     if (access_to === "/") access_to = "/index.html";
     fs.readFile(__dirname + "/public_html" + access_to, "utf-8", function(err, data) {
@@ -31,42 +39,14 @@ function createServer() {
       res.end();
     });
   }).listen(setting.port, setting.ip);
+}
+function initWSServer() {
+  var ws = websocket.server;
   var wsServer = new ws({ httpServer: server });
   wsServer.on('request', function(req) {
     req.origin = req.origin || '*';
     var websocket = req.accept(null, req.origin);
-    websocket.on('message', function(msg) {
-      console.log("msg! " + msg.utf8Data);
-      var msg_key = msg.utf8Data.split(":")[0];
-      var msg_value = msg.utf8Data.split(":")[1];
-      var isOkSend = true;
-      switch (msg_key) {
-        case "color":
-          orb.color(msg_value);
-          break;
-        case "get":
-          isOkSend = false;
-          switch (msg_value) {
-            case "color":
-              orb.getColor(function(err, data) {
-                websocket.send(data.color);
-              });
-              break;
-          }
-          break;
-        case "roll":
-          var power = msg_value.split(",")[0];
-          var angle = msg_value.split(",")[1];
-          orb.roll(parseInt(power), parseInt(angle));
-          break;
-        case "stop":
-          orb.roll(0, 0);
-          break;
-      }
-      if (isOkSend) {
-        websocket.send("OK.");
-      }
-    });
+    websocket.on('message', processWSMessage);
     websocket.on('close', function(code, desc) {
       console.log("closed");
     });
@@ -77,4 +57,37 @@ function connectSphero() {
   console.log("connected sphero");
   createServer();
 };
-readSetting();
+
+function processWSMessage(msg) {
+  console.log("msg! " + msg.utf8Data);
+  var msg_key = msg.utf8Data.split(":")[0];
+  var msg_value = msg.utf8Data.split(":")[1];
+  var isOkSend = true;
+  switch (msg_key) {
+    case "color":
+      orb.color(msg_value);
+      break;
+    case "get":
+      isOkSend = false;
+      switch (msg_value) {
+        case "color":
+          orb.getColor(function(err, data) {
+            websocket.send(data.color);
+          });
+          break;
+      }
+      break;
+    case "roll":
+      var power = msg_value.split(",")[0];
+      var angle = msg_value.split(",")[1];
+      orb.roll(parseInt(power), parseInt(angle));
+      break;
+    case "stop":
+      orb.roll(0, 0);
+      break;
+  }
+  if (isOkSend) {
+    websocket.send("OK.");
+  }
+}
+main();
